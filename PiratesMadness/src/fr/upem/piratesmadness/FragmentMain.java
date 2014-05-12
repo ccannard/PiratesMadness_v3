1c8b18b72a915650fd0f615ffa8e7e28a709a9dd
Xendir <christophe.cannard@gmail.com>
2014-05-12 13:28:05 +0200

    commit 2
    switch entre menu ok.
    Informations entre menu à finir

--- PiratesMadness/src/fr/upem/piratesmadness/FragmentMain.java
+++ PiratesMadness/src/fr/upem/piratesmadness/FragmentMain.java
@@ -1,10 +1,9 @@
 package fr.upem.piratesmadness;
-
-import java.io.ObjectInputStream.GetField;
-
+	
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -16,59 +15,77 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 public class FragmentMain extends Fragment{
-
+	private boolean sound=true;
+	
 	public FragmentMain() {
 	}
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = super.onCreateView(inflater, container, savedInstanceState);
+		super.onCreateView(inflater, container, savedInstanceState);
 		Bundle extras = this.getActivity().getIntent().getExtras();
-//
+		if(extras==null) extras = new Bundle();
 //		ImageButton bSound = (ImageButton) this.getActivity().findViewById(R.id.main_menu_sound);
 //		Log.d("PiratesMadness", "button bSound : "+bSound.getId());
-//		if(savedInstanceState!=null && savedInstanceState.getBoolean("sound"))bSound.setImageResource(R.drawable.sound_on);
+//		if(savedInstanceState!=null && savedInstanceState.getBowolean("sound"))bSound.setImageResource(R.drawable.sound_on);
 //		else bSound.setImageResource(R.drawable.sound_off);
-		setButtonOnListener(extras, v);
 		//		media = MediaPlayer.create(this, R.raw.neantitude_robin);
 		//		media.start();
 		//		media.setLooping(true);
-		return v;
+//		Log.d("PiratesMadness", "vue main");
+//		for(String k : extras.keySet()){
+//			System.out.println("Key : "+k+"; value : "+extras.getString(k));
+//		}
+		View view = inflater.inflate(R.layout.fragment_main, null);
+		Log.d("PiratesMadness", "view inflated : "+view);
+		setButtonOnListener(extras, view);
+		return view;
 	}
 
 	private void setButtonOnListener(final Bundle savedInstanceState, View v){
 		final Activity main = this.getActivity();
 		final FragmentManager fm = main.getFragmentManager();
+		final FragmentTransaction ft = fm.beginTransaction();
 		Button bPlay = (Button) v.findViewById(R.id.main_menu_play);
 		bPlay.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				fm.beginTransaction().add(android.R.id.content, new FragmentGame()).commit();  
+				Toast.makeText(getActivity(), "button game", Toast.LENGTH_SHORT).show();
+//				fm.beginTransaction().add(android.R.id.content, new FragmentGame()).commit();  
+				ft.replace(android.R.id.content, new FragmentGame());
+				ft.addToBackStack(null);
+				ft.commit();   
 			}
 		});
 		Button bSettings = (Button) v.findViewById(R.id.main_menu_settings);
 		bSettings.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Toast.makeText(getActivity(), "coucou", Toast.LENGTH_SHORT).show();
-				fm.beginTransaction().add(android.R.id.content, new FragmentSettings()).commit();  
+				Toast.makeText(getActivity(), "button settings", Toast.LENGTH_SHORT).show();
+				ft.replace(android.R.id.content, new FragmentSettings());
+				ft.addToBackStack(null);
+				ft.commit();  
 			}
 		});
 		Button bScore = (Button) v.findViewById(R.id.main_menu_score_board);
-		Log.d("PiratesMadness", "button bScore : "+bScore.getId());
 		bScore.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				fm.beginTransaction().add(android.R.id.content, new FragmentScoreBoard()).commit();  
+				Toast.makeText(getActivity(), "button score", Toast.LENGTH_SHORT).show();
+				ft.replace(android.R.id.content, new FragmentScoreBoard());
+				ft.addToBackStack(null);
+				ft.commit();  
 			}
 		});
 		final ImageButton bSound = (ImageButton) v.findViewById(R.id.main_menu_sound);
-		bScore.setOnClickListener(new OnClickListener() {
+		bSound.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				savedInstanceState.putBoolean("sound", !savedInstanceState.getBoolean("sound"));
-				if(savedInstanceState.getBoolean("sound"))bSound.setImageResource(R.drawable.sound_on);
+				Toast.makeText(getActivity(), "button sound", Toast.LENGTH_SHORT).show();
+				sound = !sound;
+				//Normalement l'enregistrement des données dans le bundle se fait dans une autre méthode
+				if(sound)bSound.setImageResource(R.drawable.sound_on);
 				else bSound.setImageResource(R.drawable.sound_off);
 			}
 		});
@@ -78,6 +95,13 @@ public class FragmentMain extends Fragment{
 	public void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		setButtonOnListener(getActivity().getIntent().getExtras(), this.getView());
+//		setButtonOnListener(getActivity().getIntent().getExtras(), this.getView());
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		outState.putBoolean("sound", sound);
 	}
 }
--- PiratesMadness/src/fr/upem/piratesmadness/FragmentSettings.java
+++ PiratesMadness/src/fr/upem/piratesmadness/FragmentSettings.java
@@ -3,10 +3,136 @@ package fr.upem.piratesmadness;
 import java.io.IOException;
 import java.util.Scanner;
 
+
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class FragmentSettings extends Fragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.fragment_settings, null);
+		setButtonOnListener(getActivity().getIntent().getExtras(), view);
+		return view;
+	}
+	
+	private void setButtonOnListener(final Bundle savedInstanceState, View v){
+		final FragmentManager fm = getFragmentManager();
+		final FragmentTransaction ft = fm.beginTransaction();
+		Button bPersonnage = (Button) v.findViewById(R.id.choose_your_character);
+		bPersonnage.setOnClickListener(new OnClickListener() {		
+			@Override
+			public void onClick(View v) {
+//				Intent character = new Intent(getApplication(), PersonnageActivity.class);
+//				character.putExtras(saveParam(savedInstanceState));
+//				startActivity(character);
+			}
+		});
+		Button bMap = (Button) v.findViewById(R.id.choose_your_map);
+		bMap.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+//				Intent map = new Intent(getApplication(), MapActivity.class);
+//				map.putExtras(saveParam(savedInstanceState));
+//				startActivity(map);
+			}
+		});
+		Button bPlay = (Button) v.findViewById(R.id.button_play);
+		bPlay.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				ft.replace(android.R.id.content, new FragmentGame());
+				ft.addToBackStack(null);
+				ft.commit();
+			}
+		});
+		Button bMenu = (Button) v.findViewById(R.id.button_menu);
+		bMenu.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Log.d("PiratesMadness", "vue fragment menu");
+				ft.replace(android.R.id.content, new FragmentMain());
+				ft.addToBackStack(null);
+				ft.commit();
+			}
+		});
+		Button bScore = (Button) v.findViewById(R.id.button_score_board);
+		bScore.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				ft.replace(android.R.id.content, new FragmentScoreBoard());
+				ft.addToBackStack(null);
+				ft.commit();
+			}
+		});
+	}
+
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		// TODO Auto-generated method stub
+		super.onSaveInstanceState(outState);
+		//utilisé saveParam pour ajouter les données
+		saveParam(outState, getView());
+	}
+	
+	public Bundle saveParam(Bundle data, View v){
+		if(data==null){
+			data = new Bundle();
+		}
+		EditText edit1 = (EditText)v.findViewById(R.id.name_player1);
+		String text = edit1.getText().toString();
+		//tester si le text vaut null lorsque l'utilisateur ne renseigne rien
+		if(text.length()==0){
+			text = new String("Player1");
+		}
+		Log.d("PiratesMadness - SettingsActivity", "text player1 vaut : "+text);
+		data.putString("namePlayerOne", text);
+
+		EditText edit2 = (EditText)v.findViewById(R.id.name_player2);
+		String text2 = edit2.getText().toString();
+		//tester si le text vaut null lorsque l'utilisateur ne renseigne rien
+		if(text2.length()==0){
+			text2 = new String("Player2");
+		}
+		Log.d("PiratesMadness - SettingsActivity", "text player2 vaut : "+text2);
+		data.putString("namePlayerTwo", text2);
+
+		RadioButton modeEasy = (RadioButton) v.findViewById(R.id.easy_mode);
+		String textMode=null;
+		if(modeEasy.isChecked()){
+			textMode = "easy";
+		}else{
+			textMode = "hard";
+		}
+		Log.d("PiratesMadness","mode : "+((modeEasy.isChecked()==true)?"easy":"hard"));
+		data.putString("mode", textMode);
+
+		RadioButton soundOn = (RadioButton) v.findViewById(R.id.button_sound_on);
+		boolean sound=false;
+		if(soundOn.isChecked()){
+			sound=true;
+		}
+		Log.d("PiratesMadness","sound : "+sound);
+		data.putBoolean("sound", sound);
+		return data;
+	}
 	
 	private CharacterItem[] createListOfCharacter(){
 		Scanner scan;
